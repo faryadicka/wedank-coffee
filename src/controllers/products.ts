78c944849a58d1cac88897a5fec3ccab58cca95e
faryadicka <faryadicka98@gmail.com>
2023-02-07 16:30:59 +0700

    set validation for transaction body

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -26,13 +26,9 @@ const createProductsController = async (req: any, res: any) => {
     const images_id = generateOTP()
     const { name, price, size, typeId, description } = req.body
     const { files } = req
-    // console.log(files)
-    // if (files.length > 0) {
       await createModel(name, Number(price), size.toUpperCase(), typeId, description, images_id)
       await imagesModel(images_id, files[0].path, files[1].path, files[2].path, files[3].path)
-      return onSuccess(res, 200, 'Create product successfully')
-    // }
-    // onFailed(res, 500, 'Please upload product images!!', null)
+    return onSuccess(res, 200, 'Create product successfully')
   } catch (error: any) {
     onFailed(res, 500, 'Internal Server Error', error.message)
   }
---
+++ src/middlewares/transactionValidate.ts
@@ -0,0 +1,5 @@
+
+const transactionValidate = (req: any, res: any, next: any) => {
+  const { size, paymentMethodType, total, name, address, phoneNumber, bankName, countryId, acquirer, productId, couponId, status, cardNumber, cardExpMonth, cardExpYear, cardCvv } = req.body
+ 
+}
