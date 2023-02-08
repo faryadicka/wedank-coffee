584dd6d4b37d75a2f1d5c965f612881919b41e3d
faryadicka <faryadicka98@gmail.com>
2023-02-08 13:14:24 +0700

    add feature get all user data for super admin

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,14 @@
 const dbUser = require("../configs/database");
 
+const getAllUserModel = () => {
+  return new Promise((resolve: any, reject: any) => {
+    dbUser.query("SELECT id, email, phone_number, address, gender, birthdate, image_profile, last_name, first_name, status FROM users", (err: any, res: any) => {
+      if (err) return reject(err)
+      return resolve(res)
+    })
+  })
+}
+
 const updateUserModel = (phoneNumber: any, address: any, gender: any, firstName: any, lastName: any, email: any, birthdate: any, image: any) => {
   return new Promise((resolve: any, reject: any) => {
     dbUser.query("UPDATE users SET phone_number=COALESCE($1, phone_number), address=COALESCE($2, address), gender=COALESCE($3, gender), first_name=COALESCE($4, first_name), last_name=COALESCE($5, last_name), birthdate=COALESCE($6, birthdate), image_profile=COALESCE($7, image_profile) WHERE email LIKE $8", [phoneNumber, address, gender, firstName, lastName, birthdate, image, email], (err: any, res: any) => {
@@ -31,4 +40,4 @@ const getUserByIdModel = (id: any) => {
   })
 }
 
-module.exports = { updateUserModel, changePasswordModel, getUserByIdModel }
+module.exports = { getAllUserModel, updateUserModel, changePasswordModel, getUserByIdModel }
