ca2ff8eb7377ea46651387fdf3ceccf2e8d5b796
faryadicka <faryadicka98@gmail.com>
2023-02-08 18:04:40 +0700

    get id for update

---
+++ src/routes/coupons.ts
@@ -0,0 +1,9 @@
+const RouteCoupon = require('express').Router()
+const { getAllCouponsController, applyCouponController } = require('../controllers/coupons')
+const { verifyToken } = require('../middlewares/verify')
+
+RouteCoupon
+  .get('/', getAllCouponsController)
+  .patch('/apply-coupon/:id', verifyToken, applyCouponController)
+
+module.exports = RouteCoupon
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,14 +3,15 @@ const authRouter = require("../routes/auth");
 const usersRouter = require("./user");
 const productsRouter = require("./products");
 const transactionRoute = require("./transactions");
-// const promosRouter = require("./promos");
+const couponRouter = require("./coupons");
 // const notifRouter = require("../routes/notif");
 
-Router.use("/auth", authRouter)
+Router
+  .use("/auth", authRouter)
   .use("/user", usersRouter)
   .use("/products", productsRouter)
   .use("/transactions", transactionRoute)
-// .use("/promos", promosRouter)
+  .use("/coupon", couponRouter)
 // .use("/notification", notifRouter);
 
 module.exports = Router;
