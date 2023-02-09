25a1101b568ded84ea42d84cbe8ecc5279f9b153
faryadicka <faryadicka98@gmail.com>
2023-02-09 17:48:28 +0700

    create model for create coupon

--- src/controllers/coupons.ts
+++ src/controllers/coupons.ts
@@ -5,13 +5,7 @@ const { getAllCouponsModel: getAllCoupons, applyCouponModel, getCouponByIdModel 
 const getAllCouponsController = async (req: any, res: any) => {
   try {
     const response = await getAllCoupons()
-    const result = response.rows.map((item: any) => {
-      const imgValues = Object.values(item).filter((i: any) => {
-        return String(i).includes('image')
-      })
-      return { id: item.id, name: item.name, image: imgValues, coupon_code: item.coupon_code, coupon_disc: item.coupon_disc, ended_at: item.ended_at, coupon_desc: item.coupon_desc }
-    })
-    onSuccess(res, 200, 'Get All Coupons Successfully', result)
+    onSuccess(res, 200, 'Get All Coupons Successfully', response.rows)
   } catch (error: any) {
     onFailed(res, 500, 'Internal Server Error', error.message)
   }
